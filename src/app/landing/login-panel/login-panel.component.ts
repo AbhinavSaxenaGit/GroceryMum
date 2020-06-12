d9cb99163f55b23f360ab838484635809ce60e86
User <User@LP-CPC-BC-177.cpc.gov.ae>
2020-06-12 19:19:30 +0400

    Added login functionality

--- src/app/landing/login-panel/login-panel.component.ts
+++ src/app/landing/login-panel/login-panel.component.ts
@@ -3,13 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login-panel',
   templateUrl: './login-panel.component.html',
-  styleUrls: ['./login-panel.component.css']
+  styleUrls: ['./login-panel.component.css'],
 })
 export class LoginPanelComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  user = [{name:'Abhinav', photo:'../../../assets/pp.jpg'}];
+
+  showLoginModal() {
+    let modal = document.getElementById('login-modal');
+    modal.style.display = 'block';
+  };
+
+  closeLoginModal() {
+    let modal = document.getElementById('login-modal');
+    let mobnumInput = document.getElementById('mobnum');
+    let otpInput = document.getElementById('otp');
+    modal.style.display = 'none';
+    otpInput.style.display = 'none';
+    mobnumInput.style.display = 'block';
+  };
+
+  toggleMobnOtp(){
+    let mobnumInput = document.getElementById('mobnum');
+    let otpInput = document.getElementById('otp');
+    let mobnumBtn = document.getElementById('mobnumBtn');
+    let otpBtn = document.getElementById('otpBtn');
+    mobnumInput.style.display = 'none';
+    otpInput.style.display = 'block';
+    mobnumBtn.style.display = 'none';
+    otpBtn.style.display = 'flex';    
   }
 
+  showUserProfile(){
+    this.closeLoginModal();
+    let loginDiv = document.getElementById('loginPanel');
+    let loggedinDiv = document.getElementById('loggedinPanel');
+    loginDiv.style.display = 'none';
+    loggedinDiv.style.display = 'block';
+  }
 }
