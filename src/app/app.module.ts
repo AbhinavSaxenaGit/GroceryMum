e723a3ea8e5c909b40c6b9d0f1138292038f449e
User <User@LP-CPC-BC-177.cpc.gov.ae>
2020-06-17 11:54:21 +0400

    Updated css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
-import { BackgroundComponent } from './top-nav-bar/background/background.component';
+import { BackgroundComponent } from './background/background.component';
 import { LoginPanelComponent } from './top-nav-bar/login-panel/login-panel.component';
 import { GlobalSearchComponent } from './top-nav-bar/global-search/global-search.component';
 import { LocationPanelComponent } from './top-nav-bar/location-panel/location-panel.component';
